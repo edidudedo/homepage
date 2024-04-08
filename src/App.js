6bc7b3662206ffc076effb305b68eb85ef789579
edidudedo <eduardoiglesius101@gmail.com>
2024-04-08 16:41:29 +0900

    First Code edited

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is edidudedo's first homepage release. (Please look forward to the next release)
         </p>
         <a
           className="App-link"
