1cf053855bde420e49b3e23a1d74b30e71977521
edidudedo <eduardoiglesius101@gmail.com>
2024-04-09 00:06:02 +0900

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './Components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is edidudedo's first homepage release. (Please look forward to the next release)
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+    <div clasName = "App">
+      <div className = "gradient__bg">
+        <Navbar />
+        {/* <Header /> */}
+      </div>
     </div>
   );
 }
