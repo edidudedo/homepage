d9c9ff812b8b2e2d663e4ca51c690bf9b6e5f44d
edidudedo <eduardoiglesius101@gmail.com>
2024-12-23 20:19:56 +0900

    Update 12-23

--- src/Assets/Collab/index.js
+++ src/Assets/Collab/index.js
@@ -2,3 +2,4 @@ export { default as Maze } from './Maze.png';
 export { default as ImageCropper } from './ImageCropper.png';
 export { default as Hackathon } from './Hackathon.png';
 export { default as IVRC } from './IVRC.png';
+export { default as URoom } from './URoom.png';
